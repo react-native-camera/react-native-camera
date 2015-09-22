3172ed70604691a0720a92fab4d9456d790a31b3
Lochlan Wansbrough <lochie@live.com>
2015-09-21 20:22:56 -0700

    Stubbed Android support.

---
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCamera.java
@@ -0,0 +1,35 @@
+package com.lwansbrough.RCTCamera;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class RCTCamera implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new RCTCamera(reactContext));
+
+    return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+  	return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+  	return Collections.emptyList();
+  }
+
+}
