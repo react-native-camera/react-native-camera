63e1db34ed52d8066ec2d61031f3d66b3a35eb8b
Ara Israelyan <juggle@juggle.ru>
2016-02-10 18:26:19 +0300

    Add type for capture options

--- Camera.js
+++ Camera.js
@@ -140,6 +140,7 @@ export default class Camera extends Component {
       audio: props.captureAudio,
       mode: props.captureMode,
       target: props.captureTarget,
+      type: props.type,
       ...options
     };
 
