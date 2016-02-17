5a52984d28e10fb26aa68f59364e3c261e724145
Ara Israelyan <juggle@juggle.ru>
2016-02-17 14:53:38 +0300

    Update Camera.js

--- Camera.js
+++ Camera.js
@@ -77,6 +77,7 @@ export default class Camera extends Component {
       PropTypes.string,
       PropTypes.number
     ]),
+    jpegQuality: PropTypes.number,
     torchMode: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.number
