963494411342a2d29ddf64ef227a1aff306e1d84
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2016-09-07 19:30:48 -0300

    Add mirrorImage support for Android

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
@@ -5,28 +5,48 @@
 
 package com.lwansbrough.RCTCamera;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaActionSound;
 import android.media.MediaRecorder;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Surface;
-import com.facebook.react.bridge.*;
 
-import javax.annotation.Nullable;
-import java.io.*;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class RCTCameraModule extends ReactContextBaseJavaModule implements MediaRecorder.OnInfoListener {
     private static final String TAG = "RCTCameraModule";
@@ -391,6 +411,19 @@ public class RCTCameraModule extends ReactContextBaseJavaModule implements Media
         return byteArray;
     }
 
+    private byte[] mirrorImage(byte[] data, Camera.Size size) {
+        ByteArrayInputStream imageStream = new ByteArrayInputStream(data);
+        Bitmap photo = BitmapFactory.decodeStream(imageStream);
+
+        Matrix m = new Matrix();
+        m.preScale(-1, 1);
+        Bitmap mirroredImage = Bitmap.createBitmap(photo, 0, 0, size.width, size.height, m, false);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        mirroredImage.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        return stream.toByteArray();
+    }
+
     @ReactMethod
     public void capture(final ReadableMap options, final Promise promise) {
         int orientation = options.hasKey("orientation") ? options.getInt("orientation") : RCTCamera.getInstance().getOrientation();
@@ -435,8 +468,14 @@ public class RCTCameraModule extends ReactContextBaseJavaModule implements Media
 
         RCTCamera.getInstance().adjustCameraRotationToDeviceOrientation(options.getInt("type"), deviceOrientation);
         camera.takePicture(null, null, new Camera.PictureCallback() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
+
+                if (options.hasKey("mirrorImage") && options.getBoolean("mirrorImage")) {
+                    data = mirrorImage(data, camera.getParameters().getPreviewSize());
+                }
+
                 camera.stopPreview();
                 camera.startPreview();
                 WritableMap response = new WritableNativeMap();
