b308d08e9a21c7c31b88990cd54b0f02a5c02842
Ron Heft <ron@thesocialstation.com>
2016-12-08 21:05:35 -0500

    Race conditions initializing flashMode (#436)
    * Fix race condition initializing Android camera
    
    * Fix race condition initializing flashMode iOS

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCamera.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCamera.java
@@ -35,7 +35,7 @@ public class RCTCamera {
     }
 
 
-    public Camera acquireCameraInstance(int type) {
+    public synchronized Camera acquireCameraInstance(int type) {
         if (null == _cameras.get(type) && null != _cameraTypeToIndex.get(type)) {
             try {
                 Camera camera = Camera.open(_cameraTypeToIndex.get(type));
@@ -206,7 +206,7 @@ public class RCTCamera {
     }
 
     public void setCaptureQuality(int cameraType, String captureQuality) {
-        Camera camera = _cameras.get(cameraType);
+        Camera camera = this.acquireCameraInstance(cameraType);
         if (camera == null) {
             return;
         }
@@ -246,7 +246,7 @@ public class RCTCamera {
     }
 
     public CamcorderProfile setCaptureVideoQuality(int cameraType, String captureQuality) {
-        Camera camera = _cameras.get(cameraType);
+        Camera camera = this.acquireCameraInstance(cameraType);
         if (camera == null) {
             return null;
         }
@@ -294,7 +294,7 @@ public class RCTCamera {
     }
 
     public void setTorchMode(int cameraType, int torchMode) {
-        Camera camera = _cameras.get(cameraType);
+        Camera camera = this.acquireCameraInstance(cameraType);
         if (null == camera) {
             return;
         }
@@ -318,7 +318,7 @@ public class RCTCamera {
     }
 
     public void setFlashMode(int cameraType, int flashMode) {
-        Camera camera = _cameras.get(cameraType);
+        Camera camera = this.acquireCameraInstance(cameraType);
         if (null == camera) {
             return;
         }
