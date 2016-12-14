56a4ab22a910654f16213678ec2f1d175fc84289
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2016-12-14 17:17:48 -0500

    Revert "Add video/audio authorization check for Android"

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
@@ -608,34 +608,6 @@ public class RCTCameraModule extends ReactContextBaseJavaModule
             }
         }
     }
-    
-    @ReactMethod
-    public void checkDeviceAuthorizationStatus(final Promise promise) {
-        if (!checkForPermission(Manifest.permission.CAMERA)) {
-            promise.resolve(false);
-        } else {
-            promise.resolve(checkForPermission(Manifest.permission.RECORD_AUDIO));
-        }
-    }
-
-    @ReactMethod
-    public void checkVideoAuthorizationStatus(final Promise promise) {
-        promise.resolve(checkForPermission(Manifest.permission.CAMERA));
-    }
-
-    @ReactMethod
-    public void checkAudioAuthorizationStatus(final Promise promise) {
-        promise.resolve(checkForPermission(Manifest.permission.RECORD_AUDIO));
-    }
-
-    private boolean checkForPermission(String permission) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-
-        int p = ContextCompat.checkSelfPermission(getReactApplicationContext(), permission);
-        return p == PackageManager.PERMISSION_GRANTED;
-    }    
 
     @ReactMethod
     public void capture(final ReadableMap options, final Promise promise) {
