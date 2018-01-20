6710179a0a8e74c8d47857ffa9d1ce696e534403
Joao Fidelis <joaogfidelis@gamil.com>
2018-01-19 22:42:50 -0200

    Implement RNCamera for iOS based on Expo code.

--- ios/RCTCameraManager.h
+++ ios/RCTCameraManager.h
@@ -1,95 +1,53 @@
 #import <React/RCTViewManager.h>
+#import <React/RCTBridgeModule.h>
 #import <AVFoundation/AVFoundation.h>
 
 @class RCTCamera;
 
-typedef NS_ENUM(NSInteger, RCTCameraAspect) {
-  RCTCameraAspectFill = 0,
-  RCTCameraAspectFit = 1,
-  RCTCameraAspectStretch = 2
-};
-
-typedef NS_ENUM(NSInteger, RCTCameraCaptureSessionPreset) {
-  RCTCameraCaptureSessionPresetLow = 0,
-  RCTCameraCaptureSessionPresetMedium = 1,
-  RCTCameraCaptureSessionPresetHigh = 2,
-  RCTCameraCaptureSessionPresetPhoto = 3,
-  RCTCameraCaptureSessionPreset480p = 4,
-  RCTCameraCaptureSessionPreset720p = 5,
-  RCTCameraCaptureSessionPreset1080p = 6,
-  RCTCameraCaptureSessionPreset4k = 7
-};
-
-typedef NS_ENUM(NSInteger, RCTCameraCaptureMode) {
-  RCTCameraCaptureModeStill = 0,
-  RCTCameraCaptureModeVideo = 1
-};
-
-typedef NS_ENUM(NSInteger, RCTCameraCaptureTarget) {
-  RCTCameraCaptureTargetMemory = 0,
-  RCTCameraCaptureTargetDisk = 1,
-  RCTCameraCaptureTargetTemp = 2,
-  RCTCameraCaptureTargetCameraRoll = 3
-};
-
-typedef NS_ENUM(NSInteger, RCTCameraOrientation) {
-  RCTCameraOrientationAuto = 0,
-  RCTCameraOrientationLandscapeLeft = AVCaptureVideoOrientationLandscapeLeft,
-  RCTCameraOrientationLandscapeRight = AVCaptureVideoOrientationLandscapeRight,
-  RCTCameraOrientationPortrait = AVCaptureVideoOrientationPortrait,
-  RCTCameraOrientationPortraitUpsideDown = AVCaptureVideoOrientationPortraitUpsideDown
-};
+static const int RCTFlashModeTorch = 3;
 
 typedef NS_ENUM(NSInteger, RCTCameraType) {
-  RCTCameraTypeFront = AVCaptureDevicePositionFront,
-  RCTCameraTypeBack = AVCaptureDevicePositionBack
+    RCTCameraTypeFront = AVCaptureDevicePositionFront,
+    RCTCameraTypeBack = AVCaptureDevicePositionBack
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraFlashMode) {
-  RCTCameraFlashModeOff = AVCaptureFlashModeOff,
-  RCTCameraFlashModeOn = AVCaptureFlashModeOn,
-  RCTCameraFlashModeAuto = AVCaptureFlashModeAuto
+    RCTCameraFlashModeOff = AVCaptureFlashModeOff,
+    RCTCameraFlashModeOn = AVCaptureFlashModeOn,
+    RCTCameraFlashModeTorch = RCTFlashModeTorch,
+    RCTCameraFlashModeAuto = AVCaptureFlashModeAuto
 };
 
-typedef NS_ENUM(NSInteger, RCTCameraTorchMode) {
-  RCTCameraTorchModeOff = AVCaptureTorchModeOff,
-  RCTCameraTorchModeOn = AVCaptureTorchModeOn,
-  RCTCameraTorchModeAuto = AVCaptureTorchModeAuto
+typedef NS_ENUM(NSInteger, RCTCameraAutoFocus) {
+    RCTCameraAutoFocusOff = AVCaptureFocusModeLocked,
+    RCTCameraAutoFocusOn = AVCaptureFocusModeContinuousAutoFocus,
 };
 
-@interface RCTCameraManager : RCTViewManager<AVCaptureMetadataOutputObjectsDelegate, AVCaptureFileOutputRecordingDelegate>
+typedef NS_ENUM(NSInteger, RCTCameraWhiteBalance) {
+    RCTCameraWhiteBalanceAuto = 0,
+    RCTCameraWhiteBalanceSunny = 1,
+    RCTCameraWhiteBalanceCloudy = 2,
+    RCTCameraWhiteBalanceFlash = 3,
+    RCTCameraWhiteBalanceShadow = 4,
+    RCTCameraWhiteBalanceIncandescent = 5,
+    RCTCameraWhiteBalanceFluorescent = 6,
+};
 
-@property (nonatomic, strong) dispatch_queue_t sessionQueue;
-@property (nonatomic, strong) AVCaptureSession *session;
-@property (nonatomic, strong) AVCaptureDeviceInput *audioCaptureDeviceInput;
-@property (nonatomic, strong) AVCaptureDeviceInput *videoCaptureDeviceInput;
-@property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
-@property (nonatomic, strong) AVCaptureMovieFileOutput *movieFileOutput;
-@property (nonatomic, strong) AVCaptureMetadataOutput *metadataOutput;
-@property (nonatomic, strong) id runtimeErrorHandlingObserver;
-@property (nonatomic, assign) NSInteger presetCamera;
-@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
-@property (nonatomic, assign) NSInteger videoTarget;
-@property (nonatomic, assign) NSInteger orientation;
-@property (nonatomic, assign) BOOL mirrorImage;
-@property (nonatomic, assign) BOOL cropToPreview;
-@property (nonatomic, strong) NSArray* barCodeTypes;
-@property (nonatomic, strong) RCTPromiseResolveBlock videoResolve;
-@property (nonatomic, strong) RCTPromiseRejectBlock videoReject;
-@property (nonatomic, strong) RCTCamera *camera;
+typedef NS_ENUM(NSInteger, RCTCameraExposureMode) {
+    RCTCameraExposureLocked = AVCaptureExposureModeLocked,
+    RCTCameraExposureAuto = AVCaptureExposureModeContinuousAutoExposure,
+    RCTCameraExposureCustom = AVCaptureExposureModeCustom,
+};
 
+typedef NS_ENUM(NSInteger, RCTCameraVideoResolution) {
+    RCTCameraVideo2160p = 0,
+    RCTCameraVideo1080p = 1,
+    RCTCameraVideo720p = 2,
+    RCTCameraVideo4x3 = 3,
+};
 
-- (void)changeOrientation:(NSInteger)orientation;
-- (AVCaptureDevice *)deviceWithMediaType:(NSString *)mediaType preferringPosition:(AVCaptureDevicePosition)position;
-- (void)capture:(NSDictionary*)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
-- (void)getFOV:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
-- (void)hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject;
-- (void)initializeCaptureSessionInput:(NSString*)type;
-- (void)stopCapture;
-- (void)startSession;
-- (void)stopSession;
-- (void)focusAtThePoint:(CGPoint) atPoint;
-- (void)zoom:(CGFloat)velocity reactTag:(NSNumber *)reactTag;
+@interface RCTCameraManager : RCTViewManager <RCTBridgeModule>
 
++ (NSDictionary *)validBarCodeTypes;
 
 @end
---
+++ ios/RCTFileSystem.m
@@ -0,0 +1,33 @@
+//
+//  RCTFileSystem.m
+//  RCTCamera
+//
+//  Created by Joao Guilherme Daros Fidelis on 19/01/18.
+//
+
+#import "RCTFileSystem.h"
+
+@implementation RCTFileSystem
+
++ (BOOL)ensureDirExistsWithPath:(NSString *)path
+{
+    BOOL isDir = NO;
+    NSError *error;
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir];
+    if (!(exists && isDir)) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:&error];
+        if (error) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
++ (NSString *)generatePathInDirectory:(NSString *)directory withExtension:(NSString *)extension
+{
+    NSString *fileName = [[[NSUUID UUID] UUIDString] stringByAppendingString:extension];
+    [RCTFileSystem ensureDirExistsWithPath:directory];
+    return [directory stringByAppendingPathComponent:fileName];
+}
+
+@end
