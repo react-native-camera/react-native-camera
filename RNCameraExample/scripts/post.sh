9a8c10f4497c9008add2d96739b0aad949be6566
Joao Fidelis <joaogfidelis@gmail.com>
2018-02-02 10:34:31 -0200

    Remove RNCameraExample

--- RNCameraExample/scripts/post.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir scripts/dirtemp
-curl -o scripts/dirtemp/temp.tar.gz 'https://dl.google.com/dl/cpdc/df83c97cbca53eaf/GoogleMobileVision-1.1.0.tar.gz'
-tar -xvzf scripts/dirtemp/temp.tar.gz -C scripts/dirtemp
-cp -r scripts/dirtemp/FaceDetector/Frameworks ios/Frameworks/FaceDetector/Frameworks
-rm -rf scripts/dirtemp
