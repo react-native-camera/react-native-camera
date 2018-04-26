f61004de623a2011e99a6a8092048b513025f5ed
Felipe Constantino <felipe.constantino@linux.autbank>
2018-04-26 14:03:20 -0300

    feat(types): add types for #1523

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,7 +17,11 @@ type FlashMode = { on: any, off: any, torch: any, auto: any };
 type CameraType = { front: any, back: any };
 type WhiteBalance = { sunny: any, cloudy: any, shadow: any, incandescent: any, fluorescent: any, auto: any };
 type BarCodeType = { aztec: any, code128: any, code39: any, code39mod43: any, code93: any, ean13: any, ean8: any, pdf417: any, qr: any, upce: any, interleaved2of5: any, itf14: any, datamatrix: any };
-type VideoQuality = { '2160p': any, '1080p': any, '720p': any, '480p': any, '4:3': any };
+type VideoQuality = {
+    '2160p': any, '1080p': any, '720p': any, '480p': any, '4:3': any;
+    /** iOS Only. Android not supported. */
+    '288p': any;
+};
 type VideoCodec = { 'H264': symbol, 'JPEG': symbol, 'HVEC': symbol, 'AppleProRes422': symbol, 'AppleProRes4444': symbol };
 
 type FaceDetectionClassifications = { all: any, none: any };
