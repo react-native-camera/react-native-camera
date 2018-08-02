65475dc05a443bc6c1be59eb94beedf0f8112d0f
Arseniy Harlanow <arsenyzp@gmail.com>
2018-08-02 18:09:12 +0300

    permission RECORD_AUDIO on android
    removed Platform 23 check

--- src/handlePermissions.js
+++ src/handlePermissions.js
@@ -16,25 +16,15 @@ export const requestPermissions = async (hasVideoAndAudio, CameraManager, permis
             message: permissionDialogMessage,
           });
         if (!hasVideoAndAudio) {
-            if (Platform.Version >= 23) {
-                return grantedCamera === PermissionsAndroid.RESULTS.GRANTED
-              } else {
-                return grantedCamera === true
-              }
+            return grantedCamera === PermissionsAndroid.RESULTS.GRANTED || grantedCamera === true;
         }
         const grantedAudio = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, {
             title: permissionDialogTitle,
             message: permissionDialogMessage,
         });
 
-        // On devices before SDK version 23, the permissions are automatically granted if they appear in the manifest,
-        // so check and request should always be true.
-        // https://github.com/facebook/react-native-website/blob/master/docs/permissionsandroid.md
-        if (Platform.Version >= 23) {
-          return grantedCamera === PermissionsAndroid.RESULTS.GRANTED && grantedAudio === PermissionsAndroid.RESULTS.GRANTED
-        } else {
-          return grantedCamera === true && grantedAudio === true
-        }
+        return (grantedCamera === PermissionsAndroid.RESULTS.GRANTED || grantedCamera === true)
+            && (grantedAudio === PermissionsAndroid.RESULTS.GRANTED || grantedAudio === true);
     }
     return true;
 }
