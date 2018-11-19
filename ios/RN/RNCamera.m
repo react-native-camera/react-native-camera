eb3665314ff79231c7f8748d19c5fa48bbf49822
Laurin Quast <laurinquast@googlemail.com>
2018-11-19 21:01:49 +0100

    fix(ios): only mute recording if mute property is set to false
    Closes #1864

--- ios/RN/RNCamera.m
+++ ios/RN/RNCamera.m
@@ -522,7 +522,7 @@ static NSDictionary *defaultFaceDetectorOptions = nil;
         }
     }
 
-    [self updateSessionAudioIsMuted:!!options[@"mute"]];
+    [self updateSessionAudioIsMuted:[options[@"mute"] boolValue]];
 
     AVCaptureConnection *connection = [self.movieFileOutput connectionWithMediaType:AVMediaTypeVideo];
     if (self.videoStabilizationMode != 0) {
