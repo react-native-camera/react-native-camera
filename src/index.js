ef1c9fc98fbae4c8a513731eed4b27fb16ec9673
Laurin Quast <laurinquast@googlemail.com>
2018-12-06 10:14:35 -0600

    Fixed type export syntax.
    Co-Authored-By: maxschmeling <max@maxschmeling.me>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Camera from './Camera';
 import RNCamera, { type Status as CameraStatus } from './RNCamera';
 import FaceDetector from './FaceDetector';
 
-export type CameraStatus;
+export type CameraStatus = CameraStatus;
 export { RNCamera, FaceDetector };
 
 export default Camera;
