7c8a2e82d222e579253142ee960208a09afe1e38
Laurin Quast <laurinquast@googlemail.com>
2018-12-06 10:17:47 -0600

    Fixed type name collision.
    Co-Authored-By: maxschmeling <max@maxschmeling.me>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Camera from './Camera';
 import RNCamera, { type Status as CameraStatus } from './RNCamera';
 import FaceDetector from './FaceDetector';
 
-export type CameraStatus = CameraStatus;
+export type CameraStatus = _CameraStatus;
 export { RNCamera, FaceDetector };
 
 export default Camera;
