4bdcbd372e9bc9d445380a035b9b2b12571bf923
Laurin Quast <laurinquast@googlemail.com>
2019-01-08 11:15:52 +0100

    fix(ios): workaround accidental breaking change (#2024)
    v1.6.0 introduced an accidental breaking change which resulted in videos being recorded without audio.
    
    Closes #2029

--- ios/RN/RNCamera.m
+++ ios/RN/RNCamera.m
@@ -531,8 +531,9 @@ static NSDictionary *defaultFaceDetectorOptions = nil;
         }
     }
 
-    if ([options valueForKey:@"mute"] != nil) {
-        [self updateSessionAudioIsMuted:[options[@"mute"] boolValue]];
+    // only update audio session when mute is not set or set to false, because otherwise there will be a flickering
+    if ([options valueForKey:@"mute"] == nil || ([options valueForKey:@"mute"] != nil && ![options[@"mute"] boolValue])) {
+        [self updateSessionAudioIsMuted:NO];
     }
 
     AVCaptureConnection *connection = [self.movieFileOutput connectionWithMediaType:AVMediaTypeVideo];
