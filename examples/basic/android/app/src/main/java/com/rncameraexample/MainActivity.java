2a4abcf60f12891a29636720920f05e04ed54865
Shashank Kushwah <shashank@myoxygen.co.uk>
2019-02-06 16:16:08 +0000

    added barcode scanner example

--- examples/basic/android/app/src/main/java/com/rncameraexample/MainActivity.java
+++ examples/basic/android/app/src/main/java/com/rncameraexample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rncameraexample;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "RNCameraExample";
     }
+
+    @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
