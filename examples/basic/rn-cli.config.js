c4d9b0a10069abca5779e9b225198c18af249ec6
Marius Reimer <reime005@gmail.com>
2019-03-23 15:59:30 +0100

    Upgrade to React Native 0.59.1

--- examples/basic/rn-cli.config.js
+++ examples/basic/rn-cli.config.js
@@ -9,8 +9,19 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 const reactNativeCameraRoot = path.resolve(__dirname, '..', '..');
 
 module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
   watchFolders: [path.resolve(__dirname, 'node_modules'), reactNativeCameraRoot],
   resolver: {
-    blacklistRE: blacklist([new RegExp(`${reactNativeCameraRoot}/examples/mlkit/.*`), new RegExp(`${reactNativeCameraRoot}/node_modules/react-native/.*`)]),
+    blacklistRE: blacklist([
+      new RegExp(`${reactNativeCameraRoot}/examples/mlkit/.*`),
+      new RegExp(`${reactNativeCameraRoot}/node_modules/react-native/.*`),
+    ]),
   },
 };
