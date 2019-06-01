c636e53c0b171161086bbe2561ffe1a98c60756c
Ronaldo Lima <ronal2do@gmail.com>
2019-06-01 13:08:55 +0200

    theme

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -96,12 +96,15 @@ const siteConfig = {
   copyright: `Copyright © ${new Date().getFullYear()}`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'atom-one-dark',
   },
-
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    'js/code-blocks-buttons.js',
+  ],
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  // scripts: ['https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
