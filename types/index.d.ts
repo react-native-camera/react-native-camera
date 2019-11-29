0b21113d15893b27dca1f4544867bf52bc019712
Kyle Johnson <1007162+kyle-johnson@users.noreply.github.com>
2019-11-29 13:08:58 -0800

    add "URL" to barcodes & export additional TS types

--- types/index.d.ts
+++ types/index.d.ts
@@ -235,7 +235,7 @@ interface Size<T = number> {
   height: T;
 }
 
-interface Barcode {
+export interface Barcode {
   bounds: {
     size: Size;
     origin: Point;
@@ -294,7 +294,7 @@ interface Barcode {
   message?: string;
 }
 
-type BarcodeType =
+export type BarcodeType =
   |"EMAIL"
   |"PHONE"
   |"CALENDAR_EVENT"
@@ -306,20 +306,21 @@ type BarcodeType =
   |"TEXT"
   |"ISBN"
   |"PRODUCT"
+  |"URL"
 
-interface Email {
+export interface Email {
   address?: string;
   body?: string;
   subject?: string;
   emailType?: "UNKNOWN" | "Work" | "Home";
 }
 
-interface Phone {
+export interface Phone {
   number?: string;
   phoneType?: "UNKNOWN" | "Work" | "Home" | "Fax" | "Mobile";
 }
 
-interface Face {
+export interface Face {
   faceID?: number;
   bounds: {
     size: Size;
@@ -343,7 +344,7 @@ interface Face {
   rollAngle?: number;
 }
 
-interface TrackedTextFeature {
+export interface TrackedTextFeature {
   type: 'block' | 'line' | 'element';
   bounds: {
     size: Size;
@@ -371,7 +372,7 @@ interface TakePictureOptions {
   forceUpOrientation?: boolean;
 }
 
-interface TakePictureResponse {
+export interface TakePictureResponse {
   width: number;
   height: number;
   uri: string;
@@ -395,7 +396,7 @@ interface RecordOptions {
   codec?: keyof VideoCodec | VideoCodec[keyof VideoCodec];
 }
 
-interface RecordResponse {
+export interface RecordResponse {
   /** Path to the video saved on your app's cache directory. */
   uri: string;
   videoOrientation: number;
