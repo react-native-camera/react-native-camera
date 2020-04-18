3324ca817cf70ef135a087fb44d205deb3b30296
Kevin Sullivan <wolverine.ks@gmail.com>
2020-04-17 20:24:59 -0700

    test: add more on-device tests

--- examples/tests/tests/index.js
+++ examples/tests/tests/index.js
@@ -1,5 +1,56 @@
-import {constants, conversionTables} from './tests';
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react';
+
+import {imports} from './imports';
 import {photos} from './photos';
 import {videos} from './videos';
 
-export const tests = [constants, conversionTables, photos, videos];
+export const tests = [
+  (tester, render) => {
+    const {describe} = tester;
+
+    const renderCamera = (props = {}) => (element) =>
+      render(
+        React.cloneElement(element, {
+          style: {height: 100, width: 100},
+          ratio: '16:9',
+          ...props,
+        }),
+        'onCameraReady',
+      );
+
+    describe('imports', () => imports(tester, renderCamera()));
+
+    describe('useCamera2Api={true}, type="front"', () => {
+      const props = {useCamera2Api: true, type: 'front'};
+
+      photos(tester, renderCamera(props));
+      videos(tester, renderCamera(props));
+    });
+
+    describe('useCamera2Api={true}, type="back"', () => {
+      const props = {useCamera2Api: true, type: 'back'};
+
+      photos(tester, renderCamera(props));
+      videos(tester, renderCamera(props));
+    });
+
+    describe('type="front"', () => {
+      const props = {type: 'front'};
+
+      photos(tester, renderCamera(props));
+      videos(tester, renderCamera(props));
+    });
+
+    describe('type="back"', () => {
+      const props = {type: 'back'};
+
+      photos(tester, renderCamera(props), render);
+      videos(tester, renderCamera(props), render);
+    });
+  },
+];
---
+++ examples/tests/tests/videos/index.js
@@ -0,0 +1,1 @@
+export {videos} from './videos';
