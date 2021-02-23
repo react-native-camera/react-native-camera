bed16e96f357f3930df7b2ae7fee1dc0f9290daf
Cristiano Coelho <48869228+cristianoccazinsp@users.noreply.github.com>
2021-02-22 21:36:17 -0300

    fix: Use empty string for undefined cameraId (#3117)
    * Make cameraId default to empty string instead of null to avoid "defaultViews"
    
    * bugfix
    
    Co-authored-by: Cristiano Coelho <cristianocca@hotmail.com>

--- android/src/main/java/com/google/android/cameraview/Camera1.java
+++ android/src/main/java/com/google/android/cameraview/Camera1.java
@@ -982,7 +982,7 @@ class Camera1 extends CameraViewImpl implements MediaRecorder.OnInfoListener,
      * This rewrites {@link #mCameraId} and {@link #mCameraInfo}.
      */
     private void chooseCamera() {
-        if(_mCameraId == null){
+        if(_mCameraId == null || _mCameraId.isEmpty()){
 
             try{
                 int count = Camera.getNumberOfCameras();
