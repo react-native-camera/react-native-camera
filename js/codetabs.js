b955793a431ef90c20e4eca2c87403f84b7c1b19
MateusAndrade <mateus.andrade47@outlook.com@users.noreply.github.com>
2021-05-12 14:19:24 +0000

    Deploy website
    Deploy website version based on 94681773d37a1dd529e487683731007a0ca4fc84

--- js/codetabs.js
+++ js/codetabs.js
@@ -1,30 +1,30 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 // Turn off ESLint for this file because it's sent down to users as-is.
+
 /* eslint-disable */
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // add event listener for all tab
-  document.querySelectorAll('.nav-link').forEach(function(el) {
-    el.addEventListener('click', function(e) {
-      const groupId = e.target.getAttribute('data-group');
+  document.querySelectorAll('.nav-link').forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      var groupId = e.target.getAttribute('data-group');
       document
-        .querySelectorAll(`.nav-link[data-group=${groupId}]`)
-        .forEach(function(el) {
+        .querySelectorAll('.nav-link[data-group='.concat(groupId, ']'))
+        .forEach(function (el) {
           el.classList.remove('active');
         });
       document
-        .querySelectorAll(`.tab-pane[data-group=${groupId}]`)
-        .forEach(function(el) {
+        .querySelectorAll('.tab-pane[data-group='.concat(groupId, ']'))
+        .forEach(function (el) {
           el.classList.remove('active');
         });
       e.target.classList.add('active');
       document
-        .querySelector(`#${e.target.getAttribute('data-tab')}`)
+        .querySelector('#'.concat(e.target.getAttribute('data-tab')))
         .classList.add('active');
     });
   });
