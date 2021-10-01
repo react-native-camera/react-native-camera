8001295e3d7aff989d8cf89c176f523f7f181e16
sermler <s.ermler@evosec.de>
2021-10-01 11:40:42 +0200

    Remove useless FlashModes

--- android/src/main/java/com/google/android/cameraview/Camera2.java
+++ android/src/main/java/com/google/android/cameraview/Camera2.java
@@ -1089,8 +1089,6 @@ class Camera2 extends CameraViewImpl implements MediaRecorder.OnInfoListener, Me
             case Constants.FLASH_ON:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_ALWAYS_FLASH);
-                mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
                 break;
             case Constants.FLASH_TORCH:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
@@ -1101,14 +1099,11 @@ class Camera2 extends CameraViewImpl implements MediaRecorder.OnInfoListener, Me
             case Constants.FLASH_AUTO:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-                mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
                 break;
             case Constants.FLASH_RED_EYE:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH_REDEYE);
-                mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
+
                 break;
         }
     }
